6a1c038d384d78f95f5b0d9382c3f73cb2a4ac47
Walafi02 <walafif@yahoo.com>
2019-11-10 22:07:40 -0300

    implementing login page

--- frontend/src/_layouts/auth/styles.js
+++ frontend/src/_layouts/auth/styles.js
@@ -1,7 +1,72 @@
 import styled from 'styled-components';
+import { darken } from 'polished';
 
 export const Wrapper = styled.div`
-  background: #ddd;
+  background: #ee4d64;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 `;
 
-export const Content = styled.div``;
+export const Content = styled.div`
+  width: 100%;
+  max-width: 320px;
+  background: #fff;
+  border-radius: 4px;
+  padding: 30px 20px;
+
+  text-align: center;
+
+  img {
+    width: 120px;
+    height: 70px;
+  }
+
+  form {
+    display: flex;
+    flex-direction: column;
+    margin-top: 10px;
+
+    label {
+      text-align: left;
+      font-weight: bold;
+      font-size: 14px;
+      text-transform: uppercase;
+      font-family: Roboto-Bold;
+      color: #444444;
+      margin-top: 15px;
+    }
+
+    span {
+      color: #ee4d64;
+      align-self: flex-start;
+      margin-top: 5px;
+      font-weight: bold;
+      font-size: 12px;
+    }
+
+    input {
+      border: 1px solid rgba(0, 0, 0, 0.2);
+      border-radius: 4px;
+      height: 40px;
+      padding: 7px 10px;
+      font-size: 12px;
+    }
+
+    button {
+      margin-top: 15px;
+      height: 40px;
+      background: #ee4d64;
+      color: #fff;
+      font-weight: bold;
+      border: 0;
+      border-radius: 4px;
+      font-size: 14px;
+
+      &:hover {
+        background: ${darken(0.03, '#ee4d64')};
+      }
+    }
+  }
+`;
--- frontend/src/pages/SignIn/index.js
+++ frontend/src/pages/SignIn/index.js
@@ -1,7 +1,44 @@
 import React from 'react';
 
-// import { Container } from './styles';
+import { Form, Input } from '@rocketseat/unform';
+
+import * as Yup from 'yup';
+
+import logo from '~/assets/logo.png';
+
+const schema = Yup.object().shape({
+  email: Yup.string()
+    .email('Insira um email valido')
+    .required('O e-mail é obrigatorio'),
+  password: Yup.string().required('A Senha é obrigatoria'),
+});
 
 export default function SignIn() {
-  return <h1>login</h1>;
+  function handleSubmit({ email, password }) {
+    console.log(email, password);
+  }
+
+  return (
+    <>
+      <img src={logo} alt="gympoint" />
+
+      <Form schema={schema} onSubmit={handleSubmit}>
+        <Input
+          label="seu e-mail"
+          name="email"
+          type="email"
+          placeholder="exemplo@email.com"
+        />
+
+        <Input
+          label="sua senha"
+          name="password"
+          type="password"
+          placeholder="********"
+        />
+
+        <button type="submit">Entrar no sistema</button>
+      </Form>
+    </>
+  );
 }
