0dd13116e169ff6a67da21ffdeb1a4e2fab68353
Walafi02 <walafif@yahoo.com>
2019-11-11 23:05:27 -0300

    including lib cors in application

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,6 +1,8 @@
 import 'dotenv/config';
 
 import express from 'express';
+import cors from 'cors';
+
 import routes from './routes';
 
 import './database';
@@ -13,6 +15,7 @@ class App {
   }
 
   middlewares() {
+    this.server.use(cors());
     this.server.use(express.json());
   }
 
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,3 +1,3 @@
 import app from './app';
 
-app.listen(3030);
+app.listen(3333);
