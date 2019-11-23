04c156309a3dccc3266f4e8ab339800fc5d52584
walai <walai@walafi-pc.meuintelbras.local>
2019-11-23 11:36:11 -0300

    configured babel-plugin-root-import

--- mobile/src/index.js
+++ mobile/src/index.js
@@ -1,12 +1,8 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import SignIn from './pages/SignIn';
 
 // import { Container } from './styles';
 
 export default function App() {
-  return (
-    <View>
-      <Text>ola mundo</Text>
-    </View>
-  );
+  return <SignIn />;
 }
