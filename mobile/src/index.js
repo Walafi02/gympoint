cd9fe40fcb3b1ced4688dcece3a225f03b37a42e
walai <walai@walafi-pc.meuintelbras.local>
2019-11-23 11:14:24 -0300

    starting the project

---
+++ mobile/src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+// import { Container } from './styles';
+
+export default function App() {
+  return (
+    <View>
+      <Text>ola mundo</Text>
+    </View>
+  );
+}
